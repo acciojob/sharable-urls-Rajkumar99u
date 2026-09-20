44ccd786d29fb39a27a039b4f4cede797213cda9
Rajkumar99u <149999582+Rajkumar99u@users.noreply.github.com>
2026-09-20 22:20:26 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,14 +1,11 @@
-// your code here
-const form = document.getElementById("form")
-const url = document.getElementById("url")
+ const form = document.getElementById("form");
+        const url = document.getElementById("url");
 
-form.addEventListner("submit",function(event){
-	event.preventDefault()
-	const name = document.getElementById("name")
-	const year = document.getElementById("year")
-	url.textContent = `https://localhost:8080/?name=${name}&year=${year}`
+        form.addEventListener("submit", function(event) {
+            event.preventDefault();
 
-	
-});
+            const name = document.getElementById("name").value;
+            const year = document.getElementById("year").value;
 
-
+            url.textContent = `https://localhost:8080/?name=${name}&year=${year}`;
+        });
